fecf78e138f69183420d3512adb246734c6e45bd
Hossein Shayesteh <hossein@pop-os.localdomain>
2021-11-28 12:31:55 +0330

    story section completed and css files organized

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -296,4 +296,30 @@ $(document).ready(function () {
   ).mouseleave(function () {
     clearTimeout(timer);
   });
+  ///////////////////////////////////////
+  //               home                //
+  //            story slider           //
+  ///////////////////////////////////////
+  //click on scroll to right
+  $(".story-right-scroll").click(function (e) {
+    e.preventDefault();
+    //hide scroll to right buttom
+    $(this).fadeOut("fast");
+    //show scroll to left buttom
+    $(".story-left-scroll").fadeIn("fast");
+    $(".story").children("ul").css("transform", "translateX(-320px)");
+  });
+  //click on scroll to left
+  $(".story-left-scroll").click(function (e) {
+    e.preventDefault();
+    //hide scroll to left buttom
+    $(this).fadeOut("fast");
+    //hide scroll to right buttom
+    $(".story-right-scroll").fadeIn("fast");
+    $(".story").children("ul").css("transform", "translateX(0)");
+  });
+  ///////////////////////////////////////
+  //               home                //
+  //            follower post          //
+  ///////////////////////////////////////
 });
